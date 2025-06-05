152ffdcd9aa22f39bfbbf0541b51a545390bdd42
Lucas de Miranda <150869038+LucasdeMiranda@users.noreply.github.com>
2025-06-05 15:01:08 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "controller/biblioteca.h"
@@ -6,7 +6,8 @@
 int main()
 {
     int op;
-    do
+    getchar();
+    while (1)
     {
         printf("Bem vindo\n");
         printf("Selecione qual aréa do sistema desesja acessar\n");
@@ -17,7 +18,7 @@ int main()
         printf("5:Profisionais\n");
         printf("6:Procedimentos\n");
         printf("7:Sair\n");
-        scanf("%d", &op);
+        scanf("%d",&op);
 
         if (op == 1)
         {
@@ -43,12 +44,15 @@ int main()
         {
             menuprocedimentos();
         }
+        else if (op == 7)
+        {
+            break;
+        }
         else if (op < 1 || op > 7)
         {
             printf("opção inválida, tente novamente\n");
         }
-
-    } while (op != 7);
+    }
 
     return 0;
 }
